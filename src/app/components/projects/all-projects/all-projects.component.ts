ea8d0946048827f38402f949603744d2dba31274
parsalemi <parsalemi@outlook.com>
2024-12-24 16:37:22 +0330

    implement projects page

--- src/app/components/projects/all-projects/all-projects.component.ts
+++ src/app/components/projects/all-projects/all-projects.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TagModule } from 'primeng/tag';
+
 @Component({
   selector: 'app-all-projects',
   standalone: true,
@@ -8,9 +11,61 @@ import { ButtonModule } from 'primeng/button';
   styleUrls: ['./all-projects.component.scss'],
   imports: [
     RouterLink,
-    ButtonModule
+    CardModule,
+    AngularSvgIconModule,
+    TagModule,
   ],
 })
 export class AllProjectsComponent {
-
+  projects: Project[] = [
+    {
+      title: 'Weather App',
+      description: 'Weather status of any city using visual crossing weather API',
+      img: 'weather',
+      techsSvg: ['angular', 'sass', 'tailwind', 'rxjs'],
+      link: 'weather',
+      ownProject: true,
+    },
+    {
+      title: 'Sudoku',
+      description: 'Sudoku game (In progress)',
+      img: 'sudoku',
+      techsSvg: ['angular', 'tailwind'],
+      link: 'sudoku',
+      ownProject: true,
+    },
+    {
+      title: 'Word guess',
+      description: 'Word guess game (5 letters) that shows the correct letter in correct position (green), corrent letter in wrong postition (yellow) and wrong letter (red)',
+      img: 'wordGuess',
+      techsSvg: ['html', 'css', 'js'],
+      link: 'wordGuess',
+      ownProject: true,
+    },
+    {
+      title: 'OMP Finex',
+      description: 'A cryptocurrency exchange',
+      img: 'ompfinex',
+      techsSvg: ['angular', 'rxjs', 'tailwind', 'svelte'],
+      link: 'https://ompfinex.com',
+      ownProject: false,
+    }
+  ]
+  handleLink(el: HTMLAnchorElement, ownProject: boolean, link: string){
+    if(ownProject){
+      el.setAttribute('routerLink', '../'+link);
+      el.setAttribute('router-link', '../'+link);
+      // el.setAttribute('href', '../'+link);
+    }else {
+      el.setAttribute('href', link);
+    }
+  }
+}
+interface Project {
+  title: string;
+  description: string;
+  img: string;
+  techsSvg: string[];
+  link: string;
+  ownProject: boolean;
 }
