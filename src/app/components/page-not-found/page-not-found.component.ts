9a4d3a2b00c14aa8c2ae046ce6dda6116a660ea6
parsalemi <parsalemi@outlook.com>
2025-01-07 05:47:26 +0330

    implement 404 page

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.scss']
 })
