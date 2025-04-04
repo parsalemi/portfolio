3cb6d3888df3d87f2d291bbb452b0792fd84283a
parsalemi <parsalemi@outlook.com>
2025-04-05 00:00:27 +0330

    add: implement star rating component

---
+++ src/app/components/projects/e-commerce/products-list/star-rating/star-rating.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-star-rating',
+  standalone: true,
+  imports: [],
+  templateUrl: './star-rating.component.html',
+  styleUrl: './star-rating.component.scss'
+})
+export class StarRatingComponent {
+  @Input() rating: number = 0;
+
+  get stars() {
+    return Array(Math.floor(this.rating)).fill(0);
+  }
+}
