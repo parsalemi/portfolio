2c8c37763b9dd27ca0f04246b7b066dacb356729
parsalemi <parsalemi@outlook.com>
2025-04-06 14:29:45 +0330

    e-commerce: cart component

--- src/app/components/projects/e-commerce/cart/cart.component.ts
+++ src/app/components/projects/e-commerce/cart/cart.component.ts
@@ -1,51 +1,69 @@
-import { AsyncPipe, JsonPipe } from '@angular/common';
-import { Component, computed, inject, OnDestroy, OnInit, signal, Signal, effect } from '@angular/core';
+import { CurrencyPipe, DecimalPipe } from '@angular/common';
+import { Component, computed, inject, OnDestroy, OnInit } from '@angular/core';
 import { CartService } from '../services/cart.service';
-import { Cart, Order } from '../models/cart.model';
+import { Cart } from '../models/cart.model';
 import { Observable, Subscription } from 'rxjs';
-import { Product } from '../models/product.model';
+import { environment } from 'src/environments/environment';
+import { Router, RouterLink } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { ToastModule } from 'primeng/toast';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss',
+  providers: [MessageService],
   imports: [
-    AsyncPipe,
-    JsonPipe
+    DecimalPipe,
+    CurrencyPipe,
+    RouterLink,
+    ToastModule
   ],
 })
 export class CartComponent implements OnInit, OnDestroy{
-  constructor(){
-
-    // effect(() => console.log(this.extendedPrice()));
-  }
-
+  constructor(private message: MessageService){}
+  env = environment;
   private _cartApi = inject(CartService);
+  router = inject(Router);
   userId!: number;
   cart$!: Observable<Cart>;
   incDecSub!: Subscription;
   delSub!: Subscription;
+  purchaseSub!: Subscription;
   
   cart = computed(() => this._cartApi.getCart());
   order = computed(() => this._cartApi.cartItems());
+  cartCount = this._cartApi.cartCount;
   extendedPrice = computed(() => this.order());
   subTotal = this._cartApi.subTotal;
   taxFee = this._cartApi.taxFee;
   deliveryFee = this._cartApi.deliveryFee;
   totalPrice = this._cartApi.totalPrice;
+  totalWeight = this._cartApi.totalWeight;
 
   deleteProduct(productId: number, userId: number){
     this.delSub = this._cartApi.deleteProduct(productId, userId).subscribe();
   }
-
   increaseQuantity(productId: number, userId: number){
     this.incDecSub = this._cartApi.increaseQuantity(productId, userId).subscribe();
   }
   decreaseQuantity(productId: number, userId: number){
     this.incDecSub = this._cartApi.decreaseQuantity(productId, userId).subscribe();
   }
-  
+  purchase(){
+    this.purchaseSub = this._cartApi.purchase(this.userId, 1).subscribe({
+      next: () => {
+        this.message.add({severity: 'success', summary: 'Purchased Successfully', detail: 'Thanks for your order', life: 3000})
+        this._cartApi.cartItems.set([]);
+        setTimeout(() => this.router.navigate(['projects/e-commerce/products']), 2000);
+      },
+      error: (err) => {
+        console.log(err);
+        this.message.add({severity: 'error', summary: 'Failed', detail: 'Please try again', life: 3000})
+      }
+    });
+  }
   ngOnInit(): void {
     const token = localStorage.getItem('token');
     if(token){
@@ -53,12 +71,10 @@ export class CartComponent implements OnInit, OnDestroy{
       this.userId = JSON.parse(atob(payload)).userId;
       this._cartApi.setUserId(this.userId);
     }
-    // for(let i = 0; i < this.order().length; i++){
-    //   this.extendedPrice.set(Number(this.order()[i].price * this.order()[i].quantity));
-    // }
   }
   ngOnDestroy(): void {
     if(this.incDecSub) this.incDecSub.unsubscribe();
     if(this.delSub) this.delSub.unsubscribe();
+    if(this.purchaseSub) this.purchaseSub.unsubscribe();
   }
 }
