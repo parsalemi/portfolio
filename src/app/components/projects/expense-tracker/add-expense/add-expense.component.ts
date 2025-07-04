3490a2f35ab47edbac00c57820fa222052687a2c
parsalemi <parsalemi@outlook.com>
2025-07-04 15:30:46 +0330

    add: implement add income/expense forms and logic

--- src/app/components/projects/expense-tracker/add-expense/add-expense.component.ts
+++ src/app/components/projects/expense-tracker/add-expense/add-expense.component.ts
@@ -1,12 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+import { Expense } from '../models/expense.model';
+import { MatIconModule } from '@angular/material/icon';
+import { CalendarModule } from 'primeng/calendar';
+import { ExpensesService } from '../services/expenses.service';
 
 @Component({
   selector: 'app-add-expense',
   standalone: true,
-  imports: [],
   templateUrl: './add-expense.component.html',
-  styleUrl: './add-expense.component.scss'
+  styleUrl: './add-expense.component.scss',
+  imports: [
+    FormsModule,
+    MatIconModule,
+    CalendarModule,
+    ReactiveFormsModule
+  ],
 })
 export class AddExpenseComponent {
+  constructor(private fb: FormBuilder) {}
+  private _ctgService = inject(CategoryService);
+  private _expenseService = inject(ExpensesService);
+  categories = computed(() => this._ctgService.categories());
+  selectedDate = '';
 
+  newExpense: Expense = {
+    subject: '',
+    amount: 0,
+    description: '',
+    date: '',
+    mode: 'expense',
+    category: {
+      id: 1,
+      name: 'Home',
+      icon: 'home',
+      isDefault: true
+    }
+  }
+  addExpense(){
+    this._expenseService.saveExpense(this.newExpense);
+    this.newExpense = {
+      subject: '',
+      amount: 0,
+      description: '',
+      date: '',
+      mode: 'expense',
+      category: {
+        id: 1,
+        name: 'Home',
+        icon: 'home',
+        isDefault: true
+      }
+    }
+  }
+  handleCategory(category: Category){
+    this.newExpense.category = category;
+  }
+  formatDate(date: Date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    
+    this.newExpense.date = `${day}-${month}-${year}`;   
+  }
+  isFormValid(): boolean {
+    return (this.newExpense.amount > 0 &&
+    this.newExpense.subject.trim() !== '' &&
+    this.newExpense.date !== '')
+  }
 }
--- src/app/components/projects/expense-tracker/add-income/add-income.component.ts
+++ src/app/components/projects/expense-tracker/add-income/add-income.component.ts
@@ -1,12 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { CategoryService } from '../services/category.service';
+import { Income } from '../models/income.model';
+import { IncomesService } from '../services/incomes.service';
+import { Category } from '../models/category.model';
+import { MatIconModule } from '@angular/material/icon';
+import { CalendarModule } from 'primeng/calendar';
 
 @Component({
   selector: 'app-add-income',
   standalone: true,
-  imports: [],
   templateUrl: './add-income.component.html',
-  styleUrl: './add-income.component.scss'
+  styleUrl: './add-income.component.scss',
+  imports: [
+    MatIconModule,
+    FormsModule,
+    CalendarModule
+  ],
 })
 export class AddIncomeComponent {
+  constructor(private fb: FormBuilder) {}
+  private _ctgService = inject(CategoryService);
+  private _incomeService = inject(IncomesService);
+  categories = computed(() => this._ctgService.categories());
+  selectedDate = '';
 
+  newIncome: Income = {
+    subject: '',
+    amount: 0,
+    date: '',
+    mode: 'income',
+    category: {
+      id: 1,
+      name: 'Home',
+      icon: 'home',
+      isDefault: true
+    }
+  }
+  addIncome(){
+    this._incomeService.saveIncome(this.newIncome);
+    this.newIncome = {
+      subject: '',
+      amount: 0,
+      date: '',
+      mode: 'income',
+      category: {
+        id: 1,
+        name: 'Home',
+        icon: 'home',
+        isDefault: true
+      }
+    }
+  }
+  handleCategory(category: Category){
+    this.newIncome.category = category;
+  }
+  formatDate(date: Date) {
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+    
+    this.newIncome.date = `${day}-${month}-${year}`;   
+  }
+  isFormValid(): boolean {
+    return (this.newIncome.amount > 0 &&
+    this.newIncome.subject.trim() !== '' &&
+    this.newIncome.date !== '')
+  }
 }
