4e8a2acb95bfdc1d510735f4d12a75ee3c3e5c58
parsalemi <parsalemi@outlook.com>
2025-07-12 19:06:11 +0330

    bug fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staticFileUrl: './assets',
-  api_url: 'https://ex-portfolio-pg.onrender.com/api',
+  ecommerce_api_url: 'https://ex-portfolio-pg.onrender.com/api',
   recaptcha: {
     sitekey: '6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc'
   }
