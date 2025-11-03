90d98513c8798ef485365ed7082b178d2f5883ca
parsalemi <parsalemi@outlook.com>
2025-11-03 12:51:41 +0330

    new api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staticFileUrl: './assets',
-  ecommerce_api_url: 'https://ex-portfolio-pg.onrender.com/api',
+  ecommerce_api_url: 'http://87.229.95.94:8000',
   recaptcha: {
     sitekey: '6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc'
   }
