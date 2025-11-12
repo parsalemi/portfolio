d816428d35ed834549dd54d46ada0c287f5e7b6e
parsalemi <parsalemi@users.noreply.github.com>
2025-11-12 11:10:21 +0000

    deploy: 8c02a93e88fffdcfedc55702f435663042a85191

---
+++ chunk-MNEJOQVS.js
@@ -0,0 +1,1 @@
+var e={production:!0,staticFileUrl:"./assets",ecommerce_api_url:"https://api.parsalemi.app/api",recaptcha:{sitekey:"6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc"}};export{e as a};
--- chunk-SZKBSXY4.js
+++
@@ -1,1 +0,0 @@
-var e={production:!0,staticFileUrl:"./assets",ecommerce_api_url:"http://api.parsalemi.app/api",recaptcha:{sitekey:"6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc"}};export{e as a};
