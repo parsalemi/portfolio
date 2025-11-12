8c02a93e88fffdcfedc55702f435663042a85191
parsalemi <parsalemi@outlook.com>
2025-11-12 14:39:31 +0330

    fix typo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   staticFileUrl: './assets',
-  ecommerce_api_url: 'http://api.parsalemi.app/api',
+  ecommerce_api_url: 'https://api.parsalemi.app/api',
   recaptcha: {
     sitekey: '6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc'
   }
