4898cf06965b7eaeb3a916e8347dd7b378289b61
parsalemi <parsalemi@outlook.com>
2025-11-20 18:18:27 +0330

    add new captcha key for dxl server

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
   staticFileUrl: './assets',
   ecommerce_api_url: 'https://api.parsalemi.app/api',
   recaptcha: {
-    sitekey: '6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc'
+    // sitekey: '6LcZzRUrAAAAAATqGh01M2Z7b8zrnOrcg5UPicPc' // github pages
+    sitekey: '6LcxChMsAAAAAAlvihWOtQkDPeZhIKec7muVL18t' // dxl
   }
 }
